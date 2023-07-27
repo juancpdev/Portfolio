bf53112f3d4066a849cb2e86410da4b608e5b22c
Juan Cruz <juan_pineda_12@hotmail.es>
2023-07-27 02:28:38 -0300

    end

--- src/js/app.js
+++ src/js/app.js
@@ -4,24 +4,46 @@ document.addEventListener('DOMContentLoaded', function() {
     subirFlecha();
     verProyectos();
     menuNav();
+    setupNavigation();
 });
 
 function modos() {
     const botonDarkMode = document.querySelector('.icono-luna');
     const botonDayMode = document.querySelector('.icono-sol');
 
-    botonDarkMode.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
+    // Cuando se carga la página, revisa si el modo oscuro está activo en el almacenamiento local
+    if (localStorage.getItem('darkMode') === 'true') {
+        document.body.classList.add('dark-mode');
         botonDarkMode.style.display = "none";
         botonDayMode.style.display = "block";
+    }
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.style.opacity = .9;
+        setTimeout(function() {
+            document.body.classList.add('dark-mode');
+            botonDarkMode.style.display = "none";
+            botonDayMode.style.display = "block";
+            document.body.style.opacity = 1;
+            // Guarda el estado del modo oscuro en el almacenamiento local
+            localStorage.setItem('darkMode', 'true');
+        }, 200);
     });
 
     botonDayMode.addEventListener('click', function() {
-        document.body.classList.remove('dark-mode');
-        botonDayMode.style.display = "none";
-        botonDarkMode.style.display = "block";
+        document.body.style.opacity = .9;
+        setTimeout(function() {
+            document.body.classList.remove('dark-mode');
+            botonDayMode.style.display = "none";
+            botonDarkMode.style.display = "block";
+            document.body.style.opacity = 1;
+            // Guarda el estado del modo oscuro en el almacenamiento local
+            localStorage.setItem('darkMode', 'false');
+        }, 200);
     });
 }
+
+
 
 function subirFlecha() {
     const flecha = document.querySelector('.flecha');
@@ -38,17 +60,30 @@ function zoomPerfil() {
     const equis = document.querySelector('.equis');
     const perfil = document.querySelector('.perfil-contenedor');
     const perfilFijado = document.querySelector('.fijado-contenedor');
+    const perfilFijado2 = document.querySelector('.perfil-fijado2');  // Agrega esta línea
 
     perfil.addEventListener('click', function() {
         perfilFijado.style.display = "block";
+        // Cambia la opacidad y la escala de perfilFijado2
+        setTimeout(() => {
+            perfilFijado2.style.opacity = "1";
+            perfilFijado2.style.transform = "scale(1)";
+        }, 0);
         document.body.classList.add('no-scroll');
     });
 
     equis.addEventListener('click', function() {
-        perfilFijado.style.display = "none";
+        // Cambia la opacidad y la escala de perfilFijado2
+        perfilFijado2.style.opacity = "0";
+        perfilFijado2.style.transform = "scale(0)";
+        // Después de que la transición se haya completado, entonces cambia display a "none" en perfilFijado
+        setTimeout(() => perfilFijado.style.display = "none", 300);  // Ajusta el tiempo de demora según la duración de tu transición
         document.body.classList.remove('no-scroll');
     });
 }
+
+
+
 
 function verProyectos() {
     const descripciones = document.querySelectorAll('.descripcion-proyectos');
@@ -74,10 +109,36 @@ function verProyectos() {
 function menuNav() {
     const btn = document.querySelector('.icono-menu');
     const nav = document.querySelector('.navegacion-mobile');
+    const navLinks = document.querySelectorAll('.navegacion-mobile li a');
 
     btn.addEventListener('click', function() {
         nav.classList.toggle('abierto');
     });
+
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', function() {
+            nav.classList.remove('abierto');
+        });
+    }
+}
+
+function setupNavigation() {
+    // Manejador de eventos de clic común
+    function handleClick(event) {
+        event.preventDefault();
+        const href = this.getAttribute('href');
+        // Comprueba si el href es "#" (logo o flecha) o un ID de fragmento (navegación)
+        if (href === '/') {
+            window.scrollTo(0, 0);
+        } else {
+            document.querySelector(href).scrollIntoView({behavior: 'smooth'});
+        }
+    }
+
+    // Agrega el manejador de eventos de clic a los enlaces de navegación, al logo y a la flecha
+    document.querySelectorAll('.logo').forEach(logo => logo.addEventListener('click', handleClick));
+    document.querySelector('.flecha').addEventListener('click', handleClick);
+    document.querySelectorAll('.contenedor-nav a').forEach(link => link.addEventListener('click', handleClick));
 }
 
 
